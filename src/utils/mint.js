f50939848852c3fce48073e07888f8b5b6d409c7
Michael M <michaelarceus96@gmail.com>
2022-11-21 23:20:13 +0800

    [INT-979] - preparing web3 call contract

--- src/utils/mint.js
+++ src/utils/mint.js
@@ -1,15 +1,17 @@
 import Web3 from "web3";
 import web3Obj from "./embedHelper";
-
-const { REACT_APP_MINTING_NFT } = process.env
+import mintABI from "config/Mint.json";
+const { REACT_APP_MINTING_NFT_SC } = process.env
 
 const minting = async (address, quantity) => {
     console.log(address, quantity, "@2");
-
+    const { upbond } = web3Obj;
+    const web3 = new Web3(upbond.provider || window.web3.currentProvider);
+    const mintContract = new web3.eth.Contract(mintABI, REACT_APP_MINTING_NFT_SC);
     try {
         
     } catch (error) {
-        
+        console.log("Error mint:", error.message);
     }
 } 
 
